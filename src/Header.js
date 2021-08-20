c728f860ff4ed4b2e726745f0314bf70fe1c5c3b
Felix Oduro Fosu <odurof99@gmail.com>
2021-08-20 23:44:07 +0200

    Header built

---
+++ src/Header.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
+import HeaderOption from "./HeaderOption";
+import HomeIcon from "@material-ui/icons/Home";
+import SupervisorAccountIcon from "@material-ui/icons/SupervisorAccount";
+import BusinessCenterIcon from "@material-ui/icons/BusinessCenter";
+import ChatIcon from "@material-ui/icons/Chat";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <img
+          src="https://image.flaticon.com/icons/png/512/174/174857.png"
+          alt=""
+        />
+
+        <div className="header__search">
+          <SearchIcon />
+          <input type="text" />
+        </div>
+      </div>
+      <div className="header__right">
+        <HeaderOption Icon={HomeIcon} title="Home" />
+        <HeaderOption Icon={SupervisorAccountIcon} title="My network" />
+        <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
+        <HeaderOption Icon={ChatIcon} title="Messaging" />
+        <HeaderOption Icon={NotificationsIcon} title="Notifications" />
+        <HeaderOption
+          avatar="https://media-exp1.licdn.com/dms/image/C5603AQErXLMQS0aPxA/profile-displayphoto-shrink_100_100/0/1588160001902?e=1634774400&v=beta&t=0feICRbQybEsh99OVQBX2zFQSUPy46u5W3zAmk-4jhQ"
+          title="Me"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
