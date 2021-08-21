b010fd77b30822b0d42b49a895a2c0a80b21874b
Felix Oduro Fosu <odurof99@gmail.com>
2021-08-21 21:42:23 +0200

    Feed: posts in descending order

--- src/Feed.js
+++ src/Feed.js
@@ -15,14 +15,16 @@ function Feed() {
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
-    db.collection("posts").onSnapshot((snapshot) =>
-      setPosts(
-        snapshot.docs.map((doc) => ({
-          id: doc.id,
-          data: doc.data(),
-        }))
-      )
-    );
+    db.collection("posts")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) =>
+        setPosts(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            data: doc.data(),
+          }))
+        )
+      );
   }, []);
 
   const sendPost = (e) => {
